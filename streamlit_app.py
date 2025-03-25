63a3aaa943ed6b3ae09e6d2a5b41dc335fb11d84
carl-leonador03 <caleonador@my.cspc.edu.ph>
2025-03-25 15:00:15 +0800

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,15 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.write(
+    "Once again, Germany took a huge L on this war."
+)
+st.write(
+    "Adolf thought fr fr 💀"
+)
+st.write(
+    "However, bro got cooked by British Forces."
+)
+st.write(
+    "W in the chat, for United Kingdom."
+)
